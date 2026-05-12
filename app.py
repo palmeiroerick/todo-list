e8750e546139480140b7bd6cccac6886ea46052d
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2026-05-12 16:57:29 -0300

    feat: implement deleting todo

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 TodoList = [
-    { "todo": "Todo1" },
-    { "todo": "Todo2" },
-    { "todo": "Todo3" },
-    { "todo": "Todo4" },
+    { "id": 0, "todo": "Todo1" },
+    { "id": 1, "todo": "Todo2" },
+    { "id": 2, "todo": "Todo3" },
+    { "id": 3, "todo": "Todo4" },
 ]
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
-@app.route("/api/todos")
+@app.route("/api/todos", methods=["GET"])
 def todo_list():
     return jsonify(TodoList), 200
 
@@ -22,5 +22,13 @@ def add_todo():
     if not (todo := request.form.get("todo")):
         return jsonify({ "error": "missing todo" }), 400
 
-    TodoList.append({"todo": todo})
+    next_id = max(t['id'] for t in TodoList) + 1 if TodoList else 0
+    TodoList.append({"id": next_id, "todo": todo})
+    return jsonify(TodoList), 200
+
+# TODO: todo_id may not exist, handle it when you have a database
+@app.route("/api/todos/delete/<int:todo_id>", methods=["DELETE"])
+def delete_todo(todo_id):
+    global TodoList
+    TodoList = [todo for todo in TodoList if todo["id"] != todo_id]
     return jsonify(TodoList), 200
--- static/app.js
+++ static/app.js
@@ -1,10 +1,10 @@
 const TodoList = document.getElementById("todo-list");
 
 function render(tasks) {
-    TodoList.innerHTML = tasks.map(task => `<li>${task.todo}</li>`).join("");
+    TodoList.innerHTML = tasks.map(task => `<li id="${task.id}"><span>${task.todo}</span><button>x</button></li>`).join("");
 }
 
-fetch("/api/todos").then(res => res.json()).then(tasks => render(tasks));
+fetch("/api/todos", { method: "GET" }).then(res => res.json()).then(tasks => render(tasks));
 
 const form = document.getElementById("add-todo");
 
@@ -14,7 +14,7 @@ form.addEventListener("submit", async (event) => {
     try {
         const response = await fetch("/api/todos/add", {
             method: "POST",
-            body: new FormData(form)
+            body: new FormData(form),
         })
 
         if (!response.ok) {
@@ -29,5 +29,26 @@ form.addEventListener("submit", async (event) => {
         form.reset();
     } catch (error) {
         console.error(error);
+    }
+})
+
+TodoList.addEventListener("click", async (event) => {
+    if (event.target.matches("button")) {
+        try {
+            const response = await fetch(`/api/todos/delete/${event.target.closest("li").id}`, {
+                method: "DELETE",
+            })
+
+            if (!response.ok) {
+                const error = await response.json();
+                throw new Error(error.error);
+            }
+
+            const tasks = await response.json();
+
+            render(tasks);
+        } catch (error) {
+            console.error(error);
+        }
     }
 })
